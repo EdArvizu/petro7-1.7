2c4aade30b0335ca1156c758d2a3d1f9e121b3c0
EdArvizu <103292489+EdArvizu@users.noreply.github.com>
2022-10-05 08:25:34 -0500

    Add files via upload

---
+++ juego/js/CLang.js
@@ -0,0 +1,27 @@
+TEXT_PRELOADER_CONTINUE = "Iniciar prueba";
+TEXT_WIN  = "Ganaste";
+TEXT_CONGRATS = "Felicidades";
+TEXT_NO_WIN    = "Sin intentos";
+TEXT_PLAY      = "Rasca y gana";
+TEXT_HELP1     = "";
+TEXT_ARE_SURE = "¿Quieres salir?";
+
+TEXT_HEAD       = "";
+TEXT_CURRENCY  = "";
+TEXT_ADVICE_WIN = "";
+TEXT_ADVICE_LOSE = "";
+
+TEXT_CREDIT = "";
+TEXT_PLUS = "";
+TEXT_MINUS = "";
+TEXT_BUYCARD = "Participar";
+TEXT_WIN_AMOUNT = "Acumulado";
+TEXT_CARD_VALUE = "";
+TEXT_PLAY_AGAIN = "";
+
+TEXT_SHARE_IMAGE = "200x200.png";
+TEXT_SHARE_TITLE = "Felicidades!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better?";
